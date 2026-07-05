472b96da28599c52153bd3bc77cd6e6176d1a9f6
s.m. mahbubul hasan <mahbubul.hasan01@northsouth.edu>
2026-07-05 11:03:55 +0600

    Updated mini task manager with timestamp

--- task.js
+++ task.js
@@ -1,25 +1,92 @@
-let addBtn = document.getElementById("addBtn");
-let taskNumber = 1;
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", addTask);
+const total = document.getElementById("total");
+const completed = document.getElementById("completed");
 
-function addTask() {
+let totalTask = 0;
+let completedTask = 0;
 
-    let input = document.getElementById("taskInput");
-    let task = input.value;
+addBtn.addEventListener("click", function () {
+
+    let task = taskInput.value.trim();
 
     if (task === "") {
         alert("Please enter a task!");
         return;
     }
 
+    // Current Time
+    const now = new Date();
+    const addedTime = now.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+
+    // Create Elements
     let li = document.createElement("li");
 
-    li.innerText = taskNumber + ". " + task;
+    let taskInfo = document.createElement("div");
+    taskInfo.classList.add("task-info");
 
-    document.getElementById("taskList").appendChild(li);
+    let taskName = document.createElement("span");
+    taskName.innerText = task;
 
-    taskNumber++;
+    let time = document.createElement("p");
+    time.innerText = "🕒 Added: " + addedTime;
 
-    input.value = "";
-}
+    taskInfo.appendChild(taskName);
+    taskInfo.appendChild(time);
+
+    // Complete Button
+    let completeBtn = document.createElement("button");
+    completeBtn.innerText = "Complete";
+
+    // Delete Button
+    let deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    // Add Elements
+    li.appendChild(taskInfo);
+    li.appendChild(completeBtn);
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    // Update Total
+    totalTask++;
+    total.innerText = totalTask;
+
+    taskInput.value = "";
+
+    // Complete Task
+    completeBtn.addEventListener("click", function () {
+
+        if (!li.classList.contains("done")) {
+
+            li.classList.add("done");
+
+            completedTask++;
+            completed.innerText = completedTask;
+
+        }
+
+    });
+
+    // Delete Task
+    deleteBtn.addEventListener("click", function () {
+
+        if (li.classList.contains("done")) {
+            completedTask--;
+            completed.innerText = completedTask;
+        }
+
+        totalTask--;
+        total.innerText = totalTask;
+
+        li.remove();
+
+    });
+
+});
